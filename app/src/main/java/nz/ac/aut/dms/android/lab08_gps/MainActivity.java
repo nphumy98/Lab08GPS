2379dfb9a70b5acc77b08afe2cdc774ea14897a7
Jimmy Nguyen <nphumy89@gmail.com>
2019-05-09 05:46:00 +1200

    Initial commit

---
+++ app/src/main/java/nz/ac/aut/dms/android/lab08_gps/MainActivity.java
@@ -0,0 +1,26 @@
+package nz.ac.aut.dms.android.lab08_gps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void getLocationManager(View view) {
+    }
+
+    public void getGPSProviderButtonOnclick(View view) {
+    }
+
+    public void isGPSEnabledButtonOnclick(View view) {
+    }
+
+    public void getGPSButtonOnclick(View view) {
+    }
+}
